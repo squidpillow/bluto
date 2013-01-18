97c85119217cda1648fa6262d5148ef9cacb24b5
squidpillow <squidpillow@gmail.com>
2013-01-17 20:24:09 -0500

    Intial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bluto::Application.config.secret_token = 'cdb344d1d5f0bef2251f7e24c3030804186c1fdfca8e9fe9bd3ced987aee38a8eaa9a4ab421fed0552c1d9366ffd98e7f2f32364b66ba49437ca4228eabd867f'
